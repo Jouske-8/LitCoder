20cd9d945c98d27b3f25fe6a5433b3661f8012e9
Jouske-8 <madhurtiwari02@gmail.com>
2025-10-28 22:27:30 +0530

    Create W_Maximize Robbery.cpp

---
+++ W_Maximize Robbery.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <algorithm>
+using namespace std;
+
+int solve(const vector<int> &nums) {
+    if(nums.empty()){
+        return 0;
+    }
+    int robpp = 0;
+    int robp = 0;
+    
+    for(int curr : nums){
+        int currMax = max(robp, curr + robpp);
+        
+        robpp = robp;
+        robp = currMax;
+    }
+    
+    return robp;
+}
+
+int main(){
+    string input;
+    getline(cin, input);
+    
+    stringstream ss(input);
+    int num;
+    vector<int> arr;
+    while(ss >> num){
+        arr.push_back(num);
+    }
+    cout<<solve(arr)<<endl;
+    return 0;
+}
